d1ad19bb4f45f10af873338d0d4d1f7849ebf6de
Asmara <asmaraviantielgracia@gmail.com>
2025-02-18 22:04:52 +0100

    upload files

---
+++ script.js
@@ -0,0 +1,19 @@
+// Smooth scrolling
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Navbar background change on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
